52fcd693dd3c0727923d7e32369e4e874be8368c
Priyanshu078 <priyanshupaliwal078@gmail.com>
2022-02-06 23:18:14 +0530

    added microphone ui in the app

--- app/src/main/java/com/example/mausam/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/mausam/fragments/SearchFragment.kt
@@ -1,14 +1,27 @@
 package com.example.mausam.fragments
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import com.example.mausam.R
 import com.example.mausam.databinding.FragmentSearchBinding
+import org.w3c.dom.Text
+import java.util.*
 
 object SearchValue{
     var cityName :String = ""
@@ -19,6 +32,7 @@ class SearchFragment : Fragment() {
 
     private var _binding:FragmentSearchBinding? = null
     private val binding get() = _binding!!
+    private lateinit var textToSpeech: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,10 +59,12 @@ class SearchFragment : Fragment() {
                 Toast.makeText(context,"Please type the city name",Toast.LENGTH_SHORT).show()
             }
         }
+        binding.imageButton.setOnClickListener {
+        }
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
